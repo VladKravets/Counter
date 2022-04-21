7d0fa07720f481b856a7f5a5b5dfebb27987e8b3
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-21 17:01:58 +0300

    fixed reducer

--- my-app/src/BLL/counter-reducer.ts
+++ my-app/src/BLL/counter-reducer.ts
@@ -1,5 +1,13 @@
-export const initialState = {
-    value: 0
+const initialState = {
+    counterValue: 0,
+    minValue: 0,
+    maxValue: 5,
+    disableBtnSet: true,
+    disableBtnReset: false,
+    disableBtnInc: false,
+    errorMessage: 'Invalid error!',
+    message: '',
+    error: false
 }
 
 type InitialStateType = typeof initialState
@@ -7,9 +15,19 @@ type InitialStateType = typeof initialState
 
 export const counterReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
-        case "INC-COUNT":
+        case INCREMENT:
             return {
-                ...state, value: state.value + 1
+                ...state,
+                counterValue: action.counterValue +1
+            }
+        case RESET:
+            return {
+                ...state,
+                counterValue: action.minValue
+            }
+        case "SET-COUNT-FROM-LS":
+            return {
+                ...state, value: action.value
             }
         default:
             return state
@@ -18,5 +36,15 @@ export const counterReducer = (state: InitialStateType = initialState, action: A
 
 
 export const incValueAC = () => ({type: 'INC-COUNT'} as const)
+export const setValueAC = (value: number) => ({
+    type: 'SET-COUNT-FROM-LS',
+    value
+} as const)
+
+
+
+
+
 export type IncrementCounterValueType = ReturnType<typeof incValueAC>
-export type ActionsType = IncrementCounterValueType
+export type SetCounterValueType = ReturnType<typeof setValueAC>
+export type ActionsType = IncrementCounterValueType | SetCounterValueType
