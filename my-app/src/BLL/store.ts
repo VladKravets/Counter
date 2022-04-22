c5b5205bba1d553dba88474d42c4ea82f21be6b8
Vlad Kravets <hazardvlad19@gmail.com>
2022-04-22 09:35:20 +0300

    fixed logic to LS

--- my-app/src/BLL/store.ts
+++ my-app/src/BLL/store.ts
@@ -1,15 +1,36 @@
-import {combineReducers, createStore} from "redux";
-import {counterReducer} from "./counter-reducer";
+import {combineReducers, createStore} from 'redux';
+import {counterReducer} from './counter-reducer';
 
-const rootReducer = combineReducers({
-    counter: counterReducer
-})
 
-export type AppStateType=ReturnType<typeof rootReducer>
-
-export const store = createStore(rootReducer)
-const initialState = {
-    value: 0
+function saveToLocalStorage<AppStateType>(state: AppStateType) {
+    try {
+        const serialisedState = JSON.stringify(state);
+        localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+        console.warn(e);
+    }
 }
 
-export type AppStoreType = typeof store
+function loadFromLocalStorage() {
+    try {
+        const serialisedState = localStorage.getItem("persistantState");
+        if (serialisedState === null) return undefined;
+        return JSON.parse(serialisedState);
+    } catch (e) {
+        console.warn(e);
+        return undefined;
+    }
+}
+
+
+export type AppStateType = ReturnType<typeof reducers>
+
+
+const reducers = combineReducers({
+    counter: counterReducer
+});
+
+
+export const store = createStore(reducers, loadFromLocalStorage())
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
